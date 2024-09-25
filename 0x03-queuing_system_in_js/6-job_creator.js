c79c78a53a3f4f267f218c278467474889d4d854
Edwin <oriokie@icloud.com>
2024-09-25 12:01:43 +0300

    added done tasks

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,22 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '564545454',
+  message: 'Hello Eddy, this is a test message',
+};
+
+const job = queue.create('push_notification_code', jobData).save((err) => {
+  if (!err) {
+    console.log(`Notification job created: ${job.id}`);
+  }
+});
+
+job
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed', () => {
+    console.log('Notification job failed');
+  });
